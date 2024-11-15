1e2e16abee63e07448447227bda0aa4f328a6131
jasperzeroes <jasperobed@gmail.com>
2024-11-15 12:21:51 +0100

    a function to perform simple calculation operations

---
+++ practice.py
@@ -0,0 +1,11 @@
+# def flatlands(n, c):
+#     #n = number of cities
+#     #c = a list of the indices where space stations exists
+
+#     for i in range(len(c)):
+#         print("City", c[i], " has a space station")
+
+# n = 5
+# c = [0,4]
+# print(flatlands(n, c))
+print("Hello world")
